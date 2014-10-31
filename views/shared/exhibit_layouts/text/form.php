f2491a57d9e4dc7630ae25f7a54acf8223410f2d
Anna Michelle <anna_michelle@msn.com>
2014-10-31 15:28:55 -0500

    Added shortcode functionality to text layout

--- views/shared/exhibit_layouts/text/form.php
+++ views/shared/exhibit_layouts/text/form.php
@@ -1,4 +1,5 @@
 <div class="block-text">
     <h4><?php echo __('Text'); ?></h4>
+    <p class="instructions"><?php echo __('This field accepts shortcodes.'); ?></p>
     <?php echo $this->exhibitFormText($block); ?>
 </div>
