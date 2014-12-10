85248af456cd70413ed6baab6228820af4e43cf8
Anna Michelle <anna_michelle@msn.com>
2014-12-10 14:15:57 -0600

    Increased length of featured exhibit snippet

--- views/shared/exhibits/single.php
+++ views/shared/exhibits/single.php
@@ -3,5 +3,5 @@
     <?php if ($exhibitImage = record_image($exhibit, 'square_thumbnail')): ?>
         <?php echo exhibit_builder_link_to_exhibit($exhibit, $exhibitImage, array('class' => 'image')); ?>
     <?php endif; ?>
-    <p><?php echo snippet_by_word_count(metadata($exhibit, 'description', array('no_escape' => true))); ?></p>
+    <p><?php echo snippet_by_word_count(metadata($exhibit, 'description', array('no_escape' => true)), 35); ?></p>
 </div>
